aee4fbb461f6248635449bfa0a86adc82bc11dc3
sebastianhaas <sebastianhaas@users.noreply.github.com>
2014-09-10 23:59:46 +0200

    Modified provision script: Added Java and Android setup scripts.

--- provision.sh
+++ provision.sh
@@ -6,4 +6,11 @@ apt-get install -y tesseract-ocr
 apt-get install -y libtesseract-dev
 apt-get install -y libleptonica-dev
 
-export TESSDATA_PREFIX=/vagrant/invoicescanner
+# Set tessdata environment variable which is currently not working
+export TESSDATA_PREFIX=/vagrant/invoicescanner/
+
+# Install Java 8
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_java8.sh && bash equip_java8.sh
+
+# Install Android
+wget --no-check-certificate https://github.com/aglover/ubuntu-equip/raw/master/equip_android.sh && bash equip_android.sh
