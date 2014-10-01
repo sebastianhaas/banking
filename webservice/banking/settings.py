03f18f8c322faf865090b428c4bdce0d47d7aa27
sebastianhaas <sebastian.haas@grape.at>
2014-10-01 11:32:13 +0200

    Added serializers for the other test models. #BK-4 work 5m

--- webservice/banking/settings.py
+++ webservice/banking/settings.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
@@ -67,7 +68,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
-          'autocommit': True,
+            'autocommit': True,
         },
     }
 }
@@ -92,5 +93,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR,  'templates'),
+    os.path.join(BASE_DIR, 'templates'),
 )
--- webservice/webservice/serializers.py
+++ webservice/webservice/serializers.py
@@ -1,8 +1,27 @@
-from webservice.models import BalanceSheetAccount
+from webservice.models import BalanceSheetAccount, NominalAccount, EquityAccount, ImbalanceAccount
 from rest_framework import serializers
 
 
-class BalanceSheetAccountSerializer(serializers.ModelSerializer):
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'code', 'created', 'description', 'is_container', 'is_root', 'name', 'notes')
+
+
+class BalanceSheetAccountSerializer(AccountSerializer):
     class Meta:
         model = BalanceSheetAccount
-        fields = ('id', 'code', 'created', 'description', 'is_container', 'is_root', 'name', 'notes')
+
+
+class NominalAccountSerializer(AccountSerializer):
+    class Meta:
+        model = NominalAccount
+
+
+class EquityAccountSerializer(AccountSerializer):
+    class Meta:
+        model = EquityAccount
+
+
+class ImbalanceAccountSerializer(AccountSerializer):
+    class Meta:
+        model = ImbalanceAccount
