d1bbe74096b58b59985be00a3a5f80d843162e8b
sebastianhaas <sebastian.haas@grape.at>
2014-10-01 11:55:53 +0200

    Added view for balance sheet account (BalanceSheetAccountList) #BK-3 work 5m

--- webservice/webservice/views.py
+++ webservice/webservice/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from webservice.models import BalanceSheetAccount
+from webservice.serializers import BalanceSheetAccountSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+
+class BalanceSheetAccountList(generics.ListAPIView):
+    queryset = BalanceSheetAccount.objects.all()
+    serializer_class = BalanceSheetAccountSerializer
