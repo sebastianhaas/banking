2ddcf74cd47da89966a1254a1da27b57221509aa
sebastianhaas <sebastian.haas@grape.at>
2014-10-01 18:55:11 +0200

    Added Location and InvoiceScan models #BK-4 work 10m

--- webservice/webservice/models.py
+++ webservice/webservice/models.py
@@ -44,9 +44,27 @@ class Tag(BasicModel):
 
 
 class Device(BasicModel):
-    name = models.CharField(max_length=50)
-    build_id = models.CharField(max_length=100)
     brand = models.CharField(max_length=100)
+    build_id = models.CharField(max_length=100)
+    description = models.TextField()
+    last_active = models.DateTimeField(auto_now=True)
     model = models.CharField(max_length=100)
+    name = models.CharField(max_length=50)
     serial = models.CharField(max_length=100)
+
+
+class InvoiceScan(BasicModel):
+    image = models.ImageField()
+
+
+class Location(BasicModel):
+    accuracy = models.FloatField(default=-1)
+    altitude = models.FloatField(default=-1)
+    bearing = models.FloatField(default=-1)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    speed = models.FloatField(default=-1)
+
+
+
 
